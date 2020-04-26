3468c48c925630a4733d7ebae704af744267e040
vinothsmart <s.vinothkanna90@gmail.com>
2020-04-26 16:20:48 +0530

    view user

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -49,7 +49,8 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::findOrFail($id);
+        return response()->json(['data' => $user], 200);
     }
 
     /**
