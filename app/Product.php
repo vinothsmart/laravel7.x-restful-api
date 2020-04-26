2e11c7bd1e21a53f15eb8d22192e82cd01a35726
vinothsmart <s.vinothkanna90@gmail.com>
2020-04-26 09:04:59 +0530

    transaction properties added

--- app/Product.php
+++ app/Product.php
@@ -9,7 +9,7 @@ class Product extends Model
     const AVAILABLE_PRODUCT = 'available';
     const UNAVAILABLE_PRODUCT = 'unavailable';
 
-    protected $fillable = ['name', 'description', 'quantity', 'status', 'image', 'seller_id'];
+    protected $fillable = ['name', 'description', 'quantity', 'status', 'image', 'seller_id',];
 
     public function isAvailable()
     {
