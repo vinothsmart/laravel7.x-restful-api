abd2e88a61206ad457d2d98d83ce385f55b59574
vinothsmart <s.vinothkanna90@gmail.com>
2020-04-26 18:19:42 +0530

    userCtrl added

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -146,6 +146,10 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $user = User::findOrFail($id);
+
+        $user->delete();
+
+        return response()->json(['data' => $user], 200);
     }
 }
