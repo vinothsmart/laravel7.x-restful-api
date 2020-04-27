fb62ba1ee6c6a40e9b3fb7882c4d2ad38c7c1761
vinothsmart <s.vinothkanna90@gmail.com>
2020-04-27 12:37:44 +0530

    all buyers

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Buyer;
 
-use App\Http\Controllers\Controller;
+use App\Buyer;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class BuyerController extends Controller
 {
@@ -14,7 +15,9 @@ class BuyerController extends Controller
      */
     public function index()
     {
-        //
+        $buyers = Buyer::has('transactions')->get();
+
+        return response()->json(['data' => $buyers], 200);
     }
 
     /**
