3049aeebf9fcde03f7caf1dda36271e38fa7e2c2
vinothsmart <s.vinothkanna90@gmail.com>
2020-04-28 18:08:05 +0530

    common error response

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -38,7 +38,7 @@ class UserController extends ApiController
         $validator = Validator::make($request->all(), $rules);
  
         if($validator->fails()){
-            return response()->json(['error' => $validator->errors()], 422);
+            return $this->errorResponse($validator->errors(), 422);
         }
 
         $data = $request->all();
@@ -85,7 +85,7 @@ class UserController extends ApiController
         $validator = Validator::make($request->all(), $rules);
  
         if($validator->fails()){
-            return response()->json(['error' => $validator->errors()], 422);
+            return $this->errorResponse($validator->errors(), 422);
         }
  
         if($request->has('name')) {
@@ -104,13 +104,13 @@ class UserController extends ApiController
  
         if($request->has('admin')) {
             if(!$user->isVerified()) {
-                return response()->json(['error' => 'Only Verified users can modify the admin field', 'code' => 409], 409);
+                return $this->errorResponse('Only Verified users can modify the admin field', 409);
             }
             $user->admin = $request->admin;
         }
  
         if(!$user->isDirty()) {
-            return response()->json(['error' => 'You need to specify a different value to update', 'code' => 422], 422);
+            return $this->errorResponse('You need to specify a different value to update', 422);
         }
  
         $user->save();
