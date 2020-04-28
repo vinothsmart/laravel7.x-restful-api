ca7658a6d959a2c4cf5aa23393fbfa367b891e74
vinothsmart <s.vinothkanna90@gmail.com>
2020-04-28 17:40:11 +0530

    created Apiresponser

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers\User;
 
 use App\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 use Illuminate\Support\Facades\Validator;
 
-class UserController extends Controller
+class UserController extends ApiController
 {
     /**
      * Display a listing of the resource.
