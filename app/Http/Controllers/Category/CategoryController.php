b44bdb93359b6d6de61cd879dd9c266f185dfaff
vinothsmart <s.vinothkanna90@gmail.com>
2020-05-01 11:24:25 +0530

    products

--- app/Http/Controllers/Category/CategoryController.php
+++ app/Http/Controllers/Category/CategoryController.php
@@ -22,16 +22,6 @@ class CategoryController extends ApiController
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -63,17 +53,6 @@ class CategoryController extends ApiController
     public function show(Category $category)
     {
         return $this->showOne($category);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Category $category)
-    {
-        //
     }
 
     /**
