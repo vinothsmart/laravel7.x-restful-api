b2f63b04af1d9e82cb0f15eed4ce55fedb6b915c
vinothsmart <s.vinothkanna90@gmail.com>
2020-05-01 12:35:19 +0530

    TransactionCatCtrl

--- routes/api.php
+++ routes/api.php
@@ -48,6 +48,10 @@ Route::resource('sellers', 'Seller\SellerController',['only' => ['index', 'show'
 
 Route::resource('transactions', 'Transaction\TransactionController',['only' => ['index', 'show']]);
 
+Route::resource('transactions.categories', 'Transaction\TransactionCategoryController',['only' => ['index']]);
+
+Route::resource('transactions.sellers', 'Transaction\TransactionSellerController',['only' => ['index']]);
+
 /**
 * Users
 */
