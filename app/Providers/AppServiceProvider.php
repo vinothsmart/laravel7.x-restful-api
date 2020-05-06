e4d83b062327eed3088cbeaff61b8ea1c498841f
vinothsmart <s.vinothkanna90@gmail.com>
2020-05-06 16:26:03 +0530

    user change verificatio mail send

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\User;
 use App\Product;
 use App\Mail\UserCreated;
+use App\Mail\UserMailChanged;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
@@ -31,6 +32,13 @@ class AppServiceProvider extends ServiceProvider
         User::created(function($user) {
             Mail::to($user)->send(new UserCreated($user));
         });
+
+        User::updated(function($user) {
+            if($user->isDirty('email')){
+                Mail::to($user)->send(new UserMailChanged($user));
+            }           
+        });
+ 
  
         Product::updated(function($product){
             if($product->quantity == 0  && $product->isAvailable()){
