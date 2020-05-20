51609baf0524b2aaca26a8771d50bebf0f89af63
vinothsmart <s.vinothkanna90@gmail.com>
2020-05-20 17:59:00 +0530

    added trasformer user

--- app/Transformers/UserTransformer.php
+++ app/Transformers/UserTransformer.php
@@ -2,6 +2,7 @@
 
 namespace App\Transformers;
 
+use App\User;
 use League\Fractal\TransformerAbstract;
 
 class UserTransformer extends TransformerAbstract
@@ -29,10 +30,17 @@ class UserTransformer extends TransformerAbstract
      *
      * @return array
      */
-    public function transform()
+    public function transform(User $user)
     {
         return [
-            //
-        ];
+            'identifier' => (int)$user->id,
+            'name' => (string)$user->name,
+            'email' => (string)$user->email,
+            'isVerified' => (int)$user->verified,
+            'isAdmin' => ($user->admin === 'true'),
+            'creationDate' => $user->created_at,
+            'lastChange' => $user->updated_at,
+            'deletedDate' => isset($user->deleted_at) ? (string) $user->deleted_at : null,
+        ]; 
     }
 }
