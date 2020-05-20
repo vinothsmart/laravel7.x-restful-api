32d20b30a272ad5740466d58e776ede9e9fc40e3
vinothsmart <s.vinothkanna90@gmail.com>
2020-05-20 17:53:19 +0530

    factories added in config

--- config/app.php
+++ config/app.php
@@ -165,7 +165,8 @@ return [
         /*
          * Package Service Providers...
          */
-
+        Spatie\Fractal\FractalServiceProvider::class,
+        
         /*
          * Application Service Providers...
          */
