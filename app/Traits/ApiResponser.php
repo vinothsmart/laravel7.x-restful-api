1196832d7fb95271680b57a0867145eaafa07026
vinothsmart <s.vinothkanna90@gmail.com>
2020-05-24 12:12:13 +0530

    pagination

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -1,8 +1,9 @@
 <?php
 namespace App\Traits;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 trait ApiResponser 
 {
@@ -27,6 +28,8 @@ trait ApiResponser
         $collection = $this->filterData($collection, $transformer);
 
         $collection = $this->sortData($collection, $transformer);
+
+        $collection = $this->paginate($collection);
 
         $collection = $this->transformData($collection, $transformer);
 
@@ -76,5 +79,25 @@ trait ApiResponser
         }
     
         return $collection;
+    }
+
+    protected function paginate(Collection $collection)
+    {
+        $page = LengthAwarePaginator::resolveCurrentPage();
+        
+        $perPage = 15;
+        if(request()->has('per_page')){
+            $perPage = (int) request()->per_page;
+        }
+    
+        $results = $collection->slice(($page - 1) * $perPage, $perPage)->values();
+    
+        $paginated = new LengthAwarePaginator($results, $collection->count(), $perPage, $page, [
+            'path' => LengthAwarePaginator::resolveCurrentPath(),
+        ]);
+    
+        $paginated->appends(request()->all());
+    
+        return $paginated;
     }
 }
