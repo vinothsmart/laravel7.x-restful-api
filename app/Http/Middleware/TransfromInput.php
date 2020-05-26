f7dd337ddfc37aa11a7b8289fc3cf046f10c4aed
vinothsmart <s.vinothkanna90@gmail.com>
2020-05-26 16:35:48 +0530

    added transformerror response

--- app/Http/Middleware/TransfromInput.php
+++ app/Http/Middleware/TransfromInput.php
@@ -22,10 +22,28 @@ class TransfromInput
         foreach ($request->request->all() as $input => $value) {
             $transformedInput[$transformer::originalAttribute($input)] = $value;
         }
-    
+
         // Then going to replace the value
         $request->replace($transformedInput);
         
-        return $next($request);
+        $response = $next($request);
+
+        if(isset($response->exception) && $response->exception instanceof ValidationException){
+            $data = $response->getData();
+
+            $transformedErrors = [];
+
+            foreach ($data->error as $field => $error) {
+                $transformedField =  $transformer::transformedAttribute($field);
+
+                $transformedErrors[$transformedField]= str_replace($field, $transformedField, $error);
+            }
+
+            $data->error = $transformedErrors;
+
+            $response->setData($data);
+        }
+
+        return $response;
     }
 }
