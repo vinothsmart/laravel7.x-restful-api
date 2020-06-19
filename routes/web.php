f6d0e2fe44b8de37623c4b9fdefe82341cbadd2c
vinothsmart <s.vinothkanna90@gmail.com>
2020-06-19 16:29:28 +0530

    auhthorized clients

--- routes/web.php
+++ routes/web.php
@@ -27,3 +27,6 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/home/my-tokens', 'HomeController@getTokens')->name('personal-tokens');
 
+Route::get('/home/my-clients', 'HomeController@getClients')->name('personal-clients');
+
+Route::get('/home/authorized-clients', 'HomeController@getAuthorizedClients')->name('authorized-clients');
